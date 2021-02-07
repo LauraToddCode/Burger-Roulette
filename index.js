3c94a7753b9040d5f53f97a2325fae3fd6487dc3
LauraToddCode <laura20todd@gmail.com>
2021-02-07 12:01:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ document.getElementById("btn").addEventListener("click", function() {
     fetch('https://foodish-api.herokuapp.com/api/images/burger')
     .then(response => response.json())
     .then(photo => {
-        document.getElementById("food").src = photo.image
+        document.getElementById("food").src = photo.image;
     })
 })
